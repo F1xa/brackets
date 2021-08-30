feb40fcb7a13b05f5dc726bf8845f446ee1608c2
F1xa <Dubineckiy@bk.ru>
2021-08-30 21:23:33 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    array.push(bracketsConfig[i].join(''));
+  }
+
+
+  for (let j = 0; j < bracketsConfig.length; j++ ) {
+    if (str.includes(array[j])) {
+      str = str.replace(array[j], '');
+      j = -1;
+    }
+  }
+  return str.length === 0;
 }
